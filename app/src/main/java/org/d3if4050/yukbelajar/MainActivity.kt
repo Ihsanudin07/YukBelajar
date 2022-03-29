2b15fd94b94b3a05f7009eed24e0386aebf57ef5
Ihsanudin_Pradana_Putra <ihsanudinpradana@gmail.com>
2022-03-29 22:16:35 +0700

    Menambahkan View Binding pada aplikasi

--- app/src/main/java/org/d3if4050/yukbelajar/MainActivity.kt
+++ app/src/main/java/org/d3if4050/yukbelajar/MainActivity.kt
@@ -2,10 +2,25 @@ package org.d3if4050.yukbelajar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.d3if4050.yukbelajar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonHitung.setOnClickListener{
+            hitungKecepatan()
+        }
+    }
+
+    private fun hitungKecepatan(){
+        Log.d("MainActivity", "Tombol diklik!")
     }
 }
