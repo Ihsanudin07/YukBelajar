4a667801384a0d8d62e34166357d3f5c7b8de0ad
Ihsanudin_Pradana_Putra <ihsanudinpradana@gmail.com>
2022-05-25 13:11:54 +0700

    Membuat tampilan grid

--- app/src/main/java/org/d3if4050/yukbelajar/ui/histori/HistoriFragment.kt
+++ app/src/main/java/org/d3if4050/yukbelajar/ui/histori/HistoriFragment.kt
@@ -3,9 +3,12 @@ package org.d3if4050.yukbelajar.ui.histori
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import org.d3if4050.yukbelajar.R
@@ -21,6 +24,7 @@ class HistoriFragment: Fragment() {
     }
     private lateinit var binding: FragmentHistoriBinding
     private lateinit var myAdapter: HistoriAdapter
+    private var isLinearLayoutManager = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,17 +50,49 @@ class HistoriFragment: Fragment() {
         })
     }
 
+    private fun chooseLayout(){
+        if(isLinearLayoutManager){
+            binding.recyclerView.layoutManager =
+                LinearLayoutManager(this.requireContext())
+        }else{
+            binding.recyclerView.layoutManager =
+                GridLayoutManager(this.requireContext(), 2)
+        }
+    }
+
+    private fun setIcon(menuItem: MenuItem){
+        if (menuItem == null) return
+
+        menuItem.icon =
+            if (isLinearLayoutManager)
+                ContextCompat.getDrawable(requireContext(),
+                R.drawable.ic_baseline_grid_view_24)
+            else ContextCompat.getDrawable(requireContext(),
+                R.drawable.ic_baseline_view_list_24)
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.histori_menu, menu)
+        val layoutButton = menu?.findItem(R.id.action_switch_layout)
+        setIcon(layoutButton)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.menu_hapus){
-            hapusData()
-            return true
+        return when(item.itemId) {
+            R.id.menu_hapus -> {
+                hapusData()
+                return true
+            }
+
+            R.id.action_switch_layout -> {
+                isLinearLayoutManager = !isLinearLayoutManager
+                chooseLayout()
+                setIcon(item)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
-        return super.onOptionsItemSelected(item)
     }
 
     private fun hapusData(){
